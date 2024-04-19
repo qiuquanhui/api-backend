c7ac31931424acc430a0cc7105b67d440221babb
quanhui <757521740@qq.com>
2024-04-19 16:47:51 +0800

    interface-取消ComponentScan注解

--- api-interface/src/main/java/com/hui/apiinterface/ApiInterfaceApplication.java
+++ api-interface/src/main/java/com/hui/apiinterface/ApiInterfaceApplication.java
@@ -2,10 +2,8 @@ package com.hui.apiinterface;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.*")
 public class ApiInterfaceApplication {
 
     public static void main(String[] args) {
